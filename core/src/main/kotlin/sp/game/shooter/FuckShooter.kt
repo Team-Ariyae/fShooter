876ae09beb90fc153cfe5ea7edefa73f425a70c5
MehraB832 <mehrabshafae@gmail.com>
2024-06-15 18:48:08 +0330

    add to git

---
+++ core/src/main/kotlin/sp/game/shooter/FuckShooter.kt
@@ -0,0 +1,73 @@
+package sp.game.shooter
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import ktx.app.clearScreen
+import ktx.assets.disposeSafely
+import ktx.assets.toInternalFile
+import ktx.async.KtxAsync
+import ktx.graphics.use
+
+class FuckShooter : KtxGame<KtxScreen>() {
+    override fun create() {
+        KtxAsync.initiate()
+
+        addScreen(FirstScreen())
+        setScreen<FirstScreen>()
+    }
+}
+
+class FirstScreen : KtxScreen {
+    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+    private val batch = SpriteBatch()
+
+    override fun render(delta: Float) {
+        clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
+        batch.use {
+            it.draw(image, 100f, 160f)
+        }
+    }
+
+    override fun dispose() {
+        image.disposeSafely()
+        batch.disposeSafely()
+    }
+}
+
+/*
+package sp.game.shooter
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class FuckShooter : ApplicationAdapter() {
+    private lateinit var batch: SpriteBatch
+    private lateinit var image: Texture
+
+    override fun create() {
+        batch = SpriteBatch()
+        image = Texture("libgdx.png")
+    }
+
+    override fun render() {
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+        batch.draw(image, 140f, 210f)
+        batch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        image.dispose()
+    }
+}
+ */
